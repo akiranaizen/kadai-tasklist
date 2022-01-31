e9beb83498735ddb8d5e89b2f96780a5799a72c2
akira.naizen <akira.naizen@weserve.com>
2022-01-31 02:18:23 +0000

    add column task

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -11,9 +11,15 @@
         </tr>
         
         <tr>
+            <th>ステータス</th>
+            <td>{{ $task->status }}</td>
+        </tr>
+        
+        <tr>
             <th>タスク</th>
             <td>{{ $task->content }}</td>
         </tr>
+        
     </table>
     
     {!! link_to_route('tasks.edit','このタスクを編集',['task' => $task->id],['class' => 'btn btn-light']) !!}
